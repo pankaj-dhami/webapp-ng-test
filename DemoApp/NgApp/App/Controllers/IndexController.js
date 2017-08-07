7fd3be9c5830ec75ec2a9a958e8868ef7f609832
Pankaj Dhami <p.dhami@agilecockpit.com>
2017-08-07 13:04:34 +0530

    commit

---
+++ DemoApp/NgApp/App/Controllers/IndexController.js
@@ -0,0 +1,12 @@
+﻿(function () {
+
+    var myApp = angular.module("myApp");
+
+    var indexController = function ($scope) {
+        $scope.message = "angular js";
+
+    }
+
+    myApp.controller('indexController', indexController);
+
+})();
---
+++ DemoApp/NgApp/App/app.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="C:\Users\admin\Source\Repos\NewRepo\DemoApp\NgApp\Scripts/angular.min.js" />
+
+
+(function() {
+    
+    var myApp = angular.module("myApp", []);
+
+
+})();
+
+
